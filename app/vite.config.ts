0fe1e22dee8c2e5aacc9b4ea2253f8c2ceeab880
SHASHVATH <shash.07x@gmail.com>
2026-02-10 16:59:29 +0530

    Change Vite base path to '/PortFolio/'
    Updated base path for Vite configuration to match repository name.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,13 +2,13 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  base: "/PortFolio/",   // IMPORTANT: repo name, case-sensitive
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
